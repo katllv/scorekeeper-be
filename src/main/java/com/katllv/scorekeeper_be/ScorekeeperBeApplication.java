67f3f52fa3ca9457dfe1638dbacc1bfcb508e933
katllv <katharinavu@hotmail.com>
2026-07-09 10:58:14 +0200

    Initialize Spring Boot project with basic configuration and PostgreSQL setup

---
+++ src/main/java/com/katllv/scorekeeper_be/ScorekeeperBeApplication.java
@@ -0,0 +1,13 @@
+package com.katllv.scorekeeper_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorekeeperBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorekeeperBeApplication.class, args);
+	}
+
+}
